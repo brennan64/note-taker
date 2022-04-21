a036da70d5688090dbed051339a8fdc50e25723f
Brennan <brennanheley@gmail.com>
2022-04-21 18:25:33 -0500

    trying to clean up file structure and starter code

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require("express");
-// const apiRoutes = require('./routes/apiRoutes');
-// const htmlRoutes = require('./routes/htmlRoutes');
-const app = express();
+const path = require("path");
 const PORT = process.env.PORT || 3001;
-app.use(express.json());
+const app = express();
+
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static("public"));
-app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
