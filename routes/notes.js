4e4f836e7792d0e049fd8f868144ec5fab13d8e4
Brennan <brennanheley@gmail.com>
2022-04-21 20:07:40 -0500

    stable

---
+++ routes/notes.js
@@ -0,0 +1,33 @@
+const note = require("express").Router();
+const { readAndAppend } = require("../helpers/fsUtils");
+const { readFromFile } = require("../helpers/fsUtils");
+const uuid = require("../helpers/uuid");
+
+note.get("/", (req, res) => {
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
+
+note.post("/", (req, res) => {
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
+    };
+
+    readAndAppend(newNote, "./db/db.json");
+
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+
+    res.json(response);
+  } else {
+    res.json("Error in posting note");
+  }
+});
+
+module.exports = note;
--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
+const api = require("./routes/index.js");
 const app = express();
-
-app.use(express.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 3001;
+const path = require("path");
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api", api);
 app.use(express.static("public"));
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
-});
 
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/notes.html"));
-});
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
 
-app.use((req, res) => {
-  res.status(404).end();
-});
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.get("*", (req, res) => {
+  res.redirect("/");
 });
+app.listen(PORT, () =>
+  console.log(`Example app listening at localhost:${PORT}`)
+);
